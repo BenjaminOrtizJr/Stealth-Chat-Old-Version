7afcd5073487043b92a1057a6faa80ceed3444fe
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2023-05-08 15:16:28 -0400

    #

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './InfoBar.css';
 import closeIcon from '../../icons/close.png';
 import onlineIcon from '../../icons/onlineIcon.png';
-import logo from "../Join/assets/logo_transparent.png"
-import userImage from '../InfoBar/assets/users.png'
+import logo from "../Join/assets/logo_transparent.png";
+import userImage from '../InfoBar/assets/users.png';
+import linkIcon from '../InfoBar/assets/link.png';
 // import settingImage from '../InfoBar/assets/settings.png'
 
 const InfoBar = ({ room, users }) => {
@@ -11,9 +12,14 @@ const InfoBar = ({ room, users }) => {
     return (
         <div className="infoBar">
             <div className="leftInnerContainer">&nbsp;
-                <img className="logo_image" src={logo} alt="logo" />
+                <a href="http://localhost:3000/"><img className="logo_image" src={logo} alt="logo" /></a>&nbsp;&nbsp;
                 <img className="onlineIcon" src={onlineIcon} alt="online" />
-                <h4>{room}</h4> 
+                <h4 className="room-name">{room}</h4>
+                <span>
+                    <button className="copyButton" onClick={() => { navigator.clipboard.writeText(`https://www.stealthchat.com | Room Name: ${room}`); alert("Link Copied!!!") }}>
+                        <img src={linkIcon} alt="link" />
+                    </button>
+                </span>
             </div>
             <div className="rightInnerContainer">
                 <img className="users_image" src={userImage} alt="users" /><span className="user_count">{users.length}</span>
