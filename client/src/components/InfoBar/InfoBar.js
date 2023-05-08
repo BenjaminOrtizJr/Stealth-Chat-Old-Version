64550d9aaeda40eaffecd212bca83d34b15730bd
BenjaminOrtizJr84 <85262543+BenjaminOrtizJr84@users.noreply.github.com>
2023-05-08 12:55:50 -0400

    #

--- client/src/components/InfoBar/InfoBar.js
+++ client/src/components/InfoBar/InfoBar.js
@@ -6,21 +6,25 @@ import logo from "../Join/assets/logo_transparent.png"
 import userImage from '../InfoBar/assets/users.png'
 // import settingImage from '../InfoBar/assets/settings.png'
 
-const InfoBar = ({ room, users }) => (
-    <div className="infoBar">
-        <div className="leftInnerContainer">&nbsp;
-                <img className="logo_image" src={logo} alt="logo" /> 
+const InfoBar = ({ room, users }) => {
+
+    return (
+        <div className="infoBar">
+            <div className="leftInnerContainer">&nbsp;
+                <img className="logo_image" src={logo} alt="logo" />
                 <img className="onlineIcon" src={onlineIcon} alt="online" />
-                <h4>{room}</h4>
-        </div>
-        <div className="rightInnerContainer">
-            <img className="users_image" src={userImage} alt="users" /><span className="user_count">{users.length}</span>
-            &nbsp;&nbsp;&nbsp;&nbsp;
-            {/* <img className="settings" src={settingImage} alt="settings" />  Coming Soon !!!!!
-            &nbsp;&nbsp;&nbsp;&nbsp; */}
-            <a href="/"><img id="close-icon"src={closeIcon} alt="close" /></a>
-        </div>
-    </div>
-)
+                <h4>{room}</h4> 
+            </div>
+            <div className="rightInnerContainer">
+                <img className="users_image" src={userImage} alt="users" /><span className="user_count">{users.length}</span>
+                &nbsp;&nbsp;&nbsp;&nbsp;
+                {/* <img className="settings" src={settingImage} alt="settings" />  Coming Soon !!!!!
+                &nbsp;&nbsp;&nbsp;&nbsp; */}
+                <a href="/"><img id="close-icon" src={closeIcon} alt="close" /></a>
+            </div>
+            </div>
+    )
+}
 
 export default InfoBar;
+
